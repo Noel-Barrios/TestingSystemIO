541432d406db3607fdacf1e5f7c04b0e831f82b8
Noel Barrios <noel.barrios@optum.com>
2019-02-18 18:14:05 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+
+namespace testingSystemIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //         Opens the specified text file, reads all the text in the file, and then closes the file. 
+            string path = @"jsonFiles/PatientMedicalProfileSearch.json";
+            string path2 = @"jsonFiles/appNames.json";
+            string path3 = @"jsonFiles/deleteThisFile";
+
+            if (!File.Exists(path2))
+            {
+                Console.WriteLine("File does NOT Exist,");
+                string createText = "This is new text for a new text file" + Environment.NewLine;
+                File.WriteAllText(path2, createText, Encoding.UTF8);
+            }
+            else
+            {
+                Console.WriteLine("The file exists!" + Environment.NewLine);
+                string stringifiedJSON = File.ReadAllText(path2);
+                Console.WriteLine(stringifiedJSON);
+
+
+            }
+
+
+        }
+    }
+}
